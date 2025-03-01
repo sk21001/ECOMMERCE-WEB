3be09dc2d22fd8d20fd077178c8017780801f387
unknown <you@example.com>
2025-03-01 15:31:50 +0530

    Login

--- src/About.js
+++ src/About.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import Home from "./Home.css";
 
 const About = () => {
   return (
-    <div>
-        <h1>CONTACT PAGE</h1>
+    <div className="a1">
+      <h1 className="ab">ABOUT US</h1>
+      <div className="ac">
+        <h4 className="ad">
+          **About Us** Welcome to Lap Store, your one-stop destination for
+          high-quality products at unbeatable prices. We are passionate about
+          providing a seamless online shopping experience, offering a wide range
+          of [Laptops] to meet all your needs. Our mission is to deliver
+          top-notch products with exceptional customer service, ensuring your
+          satisfaction every step of the way. With secure payments, fast
+          shipping, and a hassle-free return policy, shopping with us is always
+          easy and enjoyable. Join our community of happy shoppers and discover
+          the best deals today!
+        </h4>
+        <hr></hr>
+      </div>
+      <div>
+        <h3 className="ab">CONTACT DETAIL</h3>
+      </div>
+      < div className="q1">
+        <h3 className="q2">Mobile no:912912912</h3>
+        <h3 className="q2"> Email id:Lapstore@gmail.com</h3>
+        <h3 className="q2" >Insta id:Lapstore_21</h3>
+        <h3 className="q2" >Website:www.lapstore.com</h3>
+        </div>
+      
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
